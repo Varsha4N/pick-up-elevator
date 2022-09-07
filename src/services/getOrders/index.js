66f406e5cf93d70a88a93d3a0d043a772f82f3c9
Varsha4N <varshanippuleti4@gmail.com>
2022-09-07 16:00:48 +0530

    First Commit

--- src/services/getOrders/index.js
+++ src/services/getOrders/index.js
@@ -1,7 +1,5 @@
 import axios from 'axios'
 
-
-export const getOrders = () => {
-
-    return axios.get('https://jsonplaceholder.typicode.com/todos/')
+export const getOrders =()=> {
+    return axios.get( 'https://jsonplaceholder.typicode.com/todos/' )
 }
